bd58aa8c4debeb544dc1430d7296041c0a3c02d0
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-06 12:52:41 +0530

    fileupload

--- fileUpload/File.js
+++ fileUpload/File.js
@@ -1,4 +1,33 @@
-let fs= require("fs");
-let express=require("express");
+let fs = require("fs");
+let express = require("express");
+let multer = require("multer");
+const { response } = require("express");
 
-let 
+let api = express();
+
+api.use(express.static("public"));
+
+api.use(multer({ dest: "temp" }).single("file"));
+
+api.post("/imageuploadapi", function (req, res) {
+    console.log("req", req)
+    let fileObj = __dirname + "/public" + req.file.originalname;
+    fs.readFile(req.file.path, function (error, readData) {
+        var resData = ""
+        fs.writeFile(fileObj, readData, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                resData = { "message": "file uploaded successfully" }
+                res.end(JSON.stringify(resData))
+            }
+        })
+        
+
+    })
+})
+
+
+
+let server = api.listen(4000)
+console.log("server is live")
