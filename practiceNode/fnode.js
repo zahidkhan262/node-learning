5a34e8c26702f46e352ba8ffbe1d8f17f1c963b9
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-27 11:41:32 +0530

    add getdata

--- practiceNode/fnode.js
+++ practiceNode/fnode.js
@@ -1,16 +1,11 @@
 const http = require('http');
-const os = require('os');
-console.log(os.totalmem)
-
-// const hostname = '127.0.0.1';
-// const port = '4000';
-
 const server = http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200,
+        { 'Content-Type': 'text/html' });
     res.write("this is node js ")
     res.end("end program")
-}).listen(4000)
+}).listen(4000, () => {
+    console.log("server is live ")
+})
 
-
-console.log("hello")
